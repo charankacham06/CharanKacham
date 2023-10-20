975cd2f29e9f9d696d1eff4e445dbafe7e9707a3
charankacham06 <charankacham06@gmail.com>
2023-10-20 10:48:25 +0530

    Controller class commit

---
+++ src/main/java/com/cg/Controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.cg.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.entity.Product;
+import com.cg.service.ProductService;
+
+@RestController
+@RequestMapping("/productapi")
+public class ProductController {
+	@Autowired
+	public ProductService service;
+	
+	@PostMapping("/")
+	public String addProduct(@RequestBody Product p){
+    return service.addProduct(p);
+	}
+	
+	@GetMapping("/")
+	public List<Product> getAll(@RequestParam("id") int id){
+		return service.getProductList();
+	}
+
+}
